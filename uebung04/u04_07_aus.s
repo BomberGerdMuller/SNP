dd2ab598bdad6c7d7c22e3bd0b80e267868caa3a
Raphael Sack <i20029@hb.dhbw-stuttgart.de>
2021-11-04 23:30:42 +0100

    abgabe 4

---
+++ uebung04/u04_07_aus.s
@@ -0,0 +1,50 @@
+.syntax unified
+.cpu cortex-m3
+.thumb
+
+.word 0x20000400
+.word 0x080000ed
+.space 0xe4
+@ D2 = PA10
+@ D7 = PA8
+@ D13 = PA5
+.global _start
+_start:
+
+	MOV R0, #0x0
+	BL port_open
+
+	MOV R0, #0x0
+	MOV R1, #8
+	MOV R2, #1
+	BL gpio_init
+
+	MOV R0, #0x0
+	MOV R1, #10
+	MOV R2, #1
+	BL gpio_init
+
+	MOV R0, #0x0
+	MOV R1, #5
+	MOV R2, #1
+	BL gpio_init
+
+	MOV R0, #0x0
+	MOV R1, #8
+	MOV R2, #0
+	BL gpio_set
+
+	MOV R0, #0x0
+	MOV R1, #10
+	MOV R2, #0
+	BL gpio_set
+
+	MOV R0, #0x0
+	MOV R1, #5
+	MOV R2, #0
+	BL gpio_set
+
+
+loop:
+	b	loop
+
